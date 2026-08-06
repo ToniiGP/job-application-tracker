2e8c30bd60d5f6120efce8ec9e59a613365c99b8
Juan Mejorado <juan.tony1516@gmail.com>
2026-08-06 15:14:43 -0500

    Implement application retrieval endpoints

--- app/routers/applications.py
+++ app/routers/applications.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends 
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import select
 from sqlalchemy.orm import Session 
 
 from app.database import get_db 
@@ -24,3 +25,30 @@ def create_application(
 
     return application
 
+
+@router.get("/", response_model= list[ApplicationResponse])
+def get_applications(
+    db: Session = Depends(get_db),
+): 
+    statement = (
+     select(Application)
+     .order_by(Application.company_name)
+     )
+    
+    result = db.execute(statement)
+    return result.scalars().all()
+
+
+@router.get("/{application_id}", response_model= ApplicationResponse)
+def get_application(
+    application_id: int,
+    db: Session = Depends(get_db),
+): 
+    application = db.get(Application, application_id)
+    
+    if application is None: 
+        raise HTTPException(
+            status_code=404,
+            detail="Application not found", 
+        )
+    return application
