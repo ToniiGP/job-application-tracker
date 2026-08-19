f59c2e1271891bfc5403dbe45b06a3567f8df61d
Juan Mejorado <juan.tony1516@gmail.com>
2026-08-19 12:44:32 -0500

    Implemented get_interview and get_interviews functions

--- app/services/interview_service.py
+++ app/services/interview_service.py
@@ -1,7 +1,7 @@
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
-from app.models import Interview
+from app.models import Interview, Application
 from app.schemas import InterviewCreate
 from services.application_service import get_application
 
@@ -31,3 +31,46 @@ def create_interview(
     db.refresh(interview)
     
     return interview 
+
+
+def get_interviews(
+    db: Session,
+    application_id: int,
+    user_id: int,
+) -> list[Interview]:
+    
+    application = get_application(
+        db,
+        application_id,
+        user_id,
+    )
+
+    if application is None:
+        return []
+    
+    statement = select(Interview).where(
+        Interview.application_id == application_id
+    ) 
+    
+    result = db.execute(statement)
+    return result.scalars().all()
+
+
+def get_interview(
+    db: Session,
+    interview_id: int,
+    user_id: int,
+) -> Interview | None:
+    
+    statement = (select(Interview).join(
+            Application,
+            Interview.application_id == Application.id,
+        )
+        .where(
+            Interview.id == interview_id,
+            Application.user_id == user_id,
+        )
+    )
+    
+    result = db.execute(statement)
+    return result.scalars().first()
