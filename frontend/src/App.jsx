fd8662516e4d0d5aa8051bb56755c995317d2928
Juan Mejorado <juan.tony1516@gmail.com>
2026-09-04 15:44:51 -0500

    Initial login page/ui v1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,11 @@
-import Navbar from "./components/NavBar"
-import ApplicationCard from "./components/ApplicationCard"
-import {useState} from "react"
-
+import Navbar from "./components/Navbar"
+import Login from "./pages/Login"
 
 function App() {
-
-  const [showApplications, setShowApplications] = useState(true)
-
   return (
     <div>
       <Navbar />
-      <h1>Job Application Tracker</h1>
-      <p>Track your applications and interviews.</p>
-
-      <button onClick={() => setShowApplications(!showApplications)}>
-        Show/Hide Applications
-      </button>
-
-      {showApplications && (
-        <div>
-          <ApplicationCard
-            company="Google"
-            jobTitle="Software Engineer"
-            status="Applied"
-          />
-
-          <ApplicationCard
-            company="Microsoft"
-            jobTitle="Backend Engineer"
-            status="Technical Interview"
-          />
-        </div>
-      )}
-
+      <Login />
     </div>
   )
 }
